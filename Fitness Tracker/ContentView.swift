45b37de991a2f8496fe7922734368c9b1963b959
TaishengChen <t3chta01@students.oamk.fi>
2025-01-08 23:40:13 +0200

    Initial Commit

---
+++ Fitness Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fitness Tracker
+//
+//  Created by Taisheng Chen on 8.1.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fitness Tracker/Fitness_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_TrackerApp.swift
+//  Fitness Tracker
+//
+//  Created by Taisheng Chen on 8.1.2025.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
